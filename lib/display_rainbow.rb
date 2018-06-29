f6445420c7ed7e1d2e5a10578baf461e330fdb2d
wkdewey@gmail.com <wkdewey@gmail.com>
2018-06-29 20:33:38 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,9 +1,11 @@
 # Write your #display_rainbow method here
 def display_rainbow(colors)
+  rainbow = ""
   colors.length.times do |i|
     string += "#{colors[i][0].upcase}: #{colors[i]}"
     if i < color.length - 1
       string += ", "
     end
   end
+  return rainbow
 end
