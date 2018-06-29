d4cfe1a4e0efac79aeefa7c5e3552fa9f1d7ec6f
wkdewey@gmail.com <wkdewey@gmail.com>
2018-06-29 20:30:06 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,9 @@
 # Write your #display_rainbow method here
 def display_rainbow(colors)
   colors.length.times do [i]
-    string+= "#{colors[0].upcase}"
+    string+= "#{colors[i][0].upcase}: #{colors[i]}"
+    if i < color.length - 1
+      string+= ", "
+    end
   end
 end
