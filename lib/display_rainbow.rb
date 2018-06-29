53160b6ad0d48d09bf2cc68abe8ce460f8e47016
wkdewey@gmail.com <wkdewey@gmail.com>
2018-06-29 20:34:23 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -3,7 +3,7 @@ def display_rainbow(colors)
   rainbow = ""
   colors.length.times do |i|
     rainbow += "#{colors[i][0].upcase}: #{colors[i]}"
-    if i < color.length - 1
+    if i < colors.length - 1
       rainbow += ", "
     end
   end
